62daddf4c1e85432c72c362565b755bc86114b92
theprab <prabut1@tcnj.edu>
2022-03-23 17:27:13 -0400

    star position experiments

--- Assets/KinectView/Scripts/Star.cs
+++ Assets/KinectView/Scripts/Star.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f0dc8e3e84fde4b30dab770f69103c8ff2f20260636bb0f2d0ff4150943a3215
-size 1861
+oid sha256:39f65a748d093de41f2e488a7578653e9f851c128ad2ec2ffd6afc3c1e5592ae
+size 1890
