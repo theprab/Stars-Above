34b12f2f5c0bdde9c443a935d8ff340b14a6219a
theprab <prabut1@tcnj.edu>
2022-03-28 12:36:42 -0400

    viewport will now scale to size to fit stars on screen

--- Assets/KinectView/Scripts/Star.cs
+++ Assets/KinectView/Scripts/Star.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:39f65a748d093de41f2e488a7578653e9f851c128ad2ec2ffd6afc3c1e5592ae
-size 1890
+oid sha256:0aecffed9924629f6fc9118813bef16154050d9219f4cea9c06734c5e691337b
+size 2155
