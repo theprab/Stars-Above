12c7c232298304e16f34b9927b076c892b778b31
theprab <prabut1@tcnj.edu>
2022-04-23 15:56:07 -0400

    changed star and camera position to match user position relative to projector

--- Assets/KinectView/Scripts/Star.cs
+++ Assets/KinectView/Scripts/Star.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:128d8cde08522ba2b198f78d02e9cfbfbec7829232384d00b7d0023dc1d1f183
-size 1194
+oid sha256:c24b7df0ac3b6ac274539ab3af6441beae4631192907152440011c59ee9bc71d
+size 1161
